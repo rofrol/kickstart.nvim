2343cdf5cc44a9f2e337de7efbc2377974570674
Roman Frołow <rofrol@gmail.com>
2023-03-20 13:49:40 +0100

    options for splitright, splitbelow, hlsearch. mapping for :noh

--- init.lua
+++ init.lua
@@ -259,6 +259,10 @@ vim.o.completeopt = 'menuone,noselect'
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.hlsearch = true
+
 -- [[ Basic Keymaps ]]
 
 -- Keymaps for better default experience
@@ -268,6 +272,8 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+vim.keymap.set('n', '<Backspace>', ":noh<cr>", { silent = true })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
