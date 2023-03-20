2fd0b9afa581115dfd7330c62855008b5abc1281
Roman Frołow <rofrol@gmail.com>
2023-03-20 13:33:59 +0100

    theme: vscode

--- init.lua
+++ init.lua
@@ -112,11 +112,40 @@ require('lazy').setup({
     },
   },
 
-  { -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+  {
+    'Mofiqul/vscode.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      --vim.cmd.colorscheme 'vscode'
+      vim.o.background = 'light'
+
+      local c = require('vscode.colors').get_colors()
+        require('vscode').setup({
+        -- Alternatively set style in setup
+        -- style = 'light'
+
+        -- Enable transparent background
+        transparent = true,
+
+        -- Enable italic comment
+        italic_comments = false,
+
+        -- Disable nvim-tree background color
+        disable_nvimtree_bg = true,
+
+        -- Override colors (see ./lua/vscode/colors.lua)
+        color_overrides = {
+            vscLineNumber = '#FFFFFF',
+        },
+
+        -- Override highlight groups (see ./lua/vscode/theme.lua)
+        group_overrides = {
+            -- this supports the same val table as vim.api.nvim_set_hl
+            -- use colors from this colorscheme by requiring vscode.colors!
+            Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
+        }
+    })
+    require('vscode').load()
     end,
   },
 
